5c9a1d2fd73923989b0881f624466e7242282130
ingega <ingega@gmail.com>
2024-12-24 09:48:55 -0600

    new data for 4h

--- data.py
+++ data.py
@@ -7,14 +7,14 @@ current_dir = Path(__file__).resolve().parent
 parent_dir = current_dir.parent
 
 # variables and another stuf
-entrada=0.5   # ths is the minimum qty of money to use
-autoentrada=False
+entrada=0.6   # ths is the minimum qty of money to use
+autoentrada=True
 porcentajeentrada=0.075  #Ojo es 0.0492 en 25x pero para que no falle el minimal notional lo ponemos en 6%
 leverage=20
 
 timeframe=3 #esta variable controla cada cuanto se checan las órdenes
-slmax=0.03  #esta variable controla la pérdida máxima
-tp=0.01
+slmax=0.08  #esta variable controla la pérdida máxima
+tp=0.08
 sl=0.01
 
 path=""
@@ -27,7 +27,7 @@ bahias=3  # este controla el ajuste del saldoO
 
 pausa=10  # son los segundos que el sistema necesita para no empalmar las lecturas de simula.py
 
-barras=60   # son los minutos que hacen que el sistema se vaya  a empate
+barras=1440   # son los minutos que hacen que el sistema se vaya  a empate
 
 debug_mode=False
 
@@ -35,17 +35,17 @@ debug_mode=False
 # gap=0.03
 # distance=0.015
 # hour=8
-bet=0.01
+bet=0.04
 time=20  # this is for prevent loops in each4hrs()
-a_size = 0.002
-b_size = 1
-c_size = 0.04
+a_size = 0.01
+b_size = 0.6
+c_size = 0.4
 
 # config for time
-hours = 1  # 1 raise zero in preview
-minutes = 5
+hours = 4  # 1 raise zero in preview
+minutes = 60
 seconds = 40
 
-interval="5m"
+interval="4h"
 
 # reverse=False  # in this system is doesn't matter because is double bet
